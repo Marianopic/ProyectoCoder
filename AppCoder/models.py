d1fa5df7cb560d8c5e45ffdf6e6ad9bca1a323b4
Marianopic <46465080+Marianopic@users.noreply.github.com>
2022-10-03 19:18:06 -0300

    Update models.py

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,26 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    
+    nombre = models.CharField(max_lenght=40)
+    camada = models.IntegerField()
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_lenght=30)
+    apellido = models.CharField(max_lenght=30)
+    email = models.EmailField(max_lenght=30)
+    
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_lenght=30)
+    apellido = models.CharField(max_lenght=30)
+    email = models.EmailField(max_lenght=30)
+    
+    
+class Entregable(models.Model):
+    nombre = models.CharField(max_lenght=40)
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()
