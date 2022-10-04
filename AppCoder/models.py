232caa55f85a13843d241ada8f9582f5efcbbd9c
Marianopic <46465080+Marianopic@users.noreply.github.com>
2022-10-04 11:19:34 -0300

    Nuevo intento

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,23 +4,29 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
     
-    nombre = models.CharField(max_lenght=40)
+    nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
 
 class Estudiante(models.Model):
-    nombre = models.CharField(max_lenght=30)
-    apellido = models.CharField(max_lenght=30)
-    email = models.EmailField(max_lenght=30)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
     
     
 class Profesor(models.Model):
-    nombre = models.CharField(max_lenght=30)
-    apellido = models.CharField(max_lenght=30)
-    email = models.EmailField(max_lenght=30)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
     
     
 class Entregable(models.Model):
-    nombre = models.CharField(max_lenght=40)
+    nombre = models.CharField(max_length=30)
     fecha_de_entrega = models.DateField()
     entregado = models.BooleanField()
+    
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=40)
+    parentezco = models.CharField(max_length=40)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
